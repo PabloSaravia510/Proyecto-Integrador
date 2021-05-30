9deea9846c5341c5bd015fadd394a6bbbf12323f
PabloSaravia510 <70932814+PabloSaravia510@users.noreply.github.com>
2021-05-30 17:52:24 -0500

    Delete School_Manager directory

--- School_Manager/src/main/java/net/spring/intranet/entidad/Seccion/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://entidad.intranet.spring.net/xsd", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package net.spring.intranet.entidad.Seccion;
