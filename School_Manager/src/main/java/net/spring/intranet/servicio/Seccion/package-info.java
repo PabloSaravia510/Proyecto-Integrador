8aeafce1bec33db49387f642724d29c556133467
PabloSaravia510 <70932814+PabloSaravia510@users.noreply.github.com>
2021-06-10 22:25:38 -0500

    Delete School_Manager directory

--- School_Manager/src/main/java/net/spring/intranet/servicio/Seccion/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- * 		Please Type your service description here
- * 	
- * 
- */
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://service.intranet.spring.net", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package net.spring.intranet.servicio.Seccion;
